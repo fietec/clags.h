d1443b3dcce2ef48b29067dbbdf23c3a76b8d76d
Constantijn de Meer <cdemeer@fim.uni-passau.de>
2025-10-23 00:14:59 +0200

    add long and short flags, make help optional

--- clags.h
+++ clags.h
@@ -39,16 +39,19 @@ typedef struct{
 } clags_req_t;
 
 typedef struct{
-    const char *flag;
+    const char *short_flag;
+    const char *long_flag;
     char **value;
     const char *field_name;
     const char *description;
 } clags_opt_t;
 
 typedef struct{
-    const char *flag;
+    const char *short_flag;
+    const char *long_flag;
     bool *value;
     const char *description;
+    bool exit;
 } clags_flag_t;
 
 typedef struct{
@@ -75,15 +78,18 @@ typedef struct{
     };
 } clags_arg_t;
 
+#define CLAGS_USAGE_ALIGNMENT -24
+
 
 #define clags_required(n, val, desc) (clags_arg_t) {.type=Clags_Required, .req=(clags_req_t){.name=(n), .value=(val), .description=(desc)}}
-#define clags_optional(f, val, f_name, desc) (clags_arg_t) {.type=Clags_Optional, .opt=(clags_opt_t){.flag=(f), .value=(val), .description=(desc), .field_name=(f_name)}}
-#define clags_flag(f, val, desc) (clags_arg_t) {.type=Clags_Flag, .flag=(clags_flag_t){.flag=(f), .value=(val), .description=(desc)}}
+#define clags_optional(sf, lf, val, f_name, desc) (clags_arg_t) {.type=Clags_Optional, .opt=(clags_opt_t){.short_flag=(sf), .long_flag=(lf), .value=(val), .description=(desc), .field_name=(f_name)}}
+#define clags_flag(sf, lf, val, desc, ex) (clags_arg_t) {.type=Clags_Flag, .flag=(clags_flag_t){.short_flag=(sf), .long_flag=(lf), .value=(val), .description=(desc), .exit=(ex)}}
+#define clags_flag_help(val) clags_flag("-h", "--help", val, "print this help dialog", true)
 
 #define clags_arr_len(arr) (sizeof(arr)/sizeof(arr[0]))
 
-#define clags_parse(argc, argv, args, help) _clags_parse((argc), (argv), (help), (args), clags_arr_len(args))
-bool _clags_parse(int argc, char **argv, bool *help, clags_arg_t *args, size_t arg_count);
+#define clags_parse(argc, argv, args) _clags_parse((argc), (argv), (args), clags_arr_len(args))
+bool _clags_parse(int argc, char **argv, clags_arg_t *args, size_t arg_count);
 
 #define clags_usage(pn, args) _clags_usage((pn), (args), clags_arr_len(args))
 void _clags_usage(const char *program_name, clags_arg_t *args, size_t arg_count);
@@ -112,46 +118,56 @@ void _clags_sort_args(clags_args_t *args, clags_arg_t *_args, size_t arg_count)
     }
 }
 
-bool _clags_parse(int argc, char **argv, bool *help, clags_arg_t *_args, size_t arg_count)
+bool _clags_parse(int argc, char **argv, clags_arg_t *_args, size_t arg_count)
 {
     clags_req_t required[arg_count];
     clags_opt_t optional[arg_count];
     clags_flag_t flags[arg_count];
 
     clags_args_t args = {.required=required, .optional=optional, .flags=flags};
-
+    
     _clags_sort_args(&args, _args, arg_count);
     
     size_t required_found = 0;
     for (size_t index=1; index<(size_t)argc; ++index){
         char *arg = argv[index];
         
-        if (help != NULL && (strcmp(arg, "-h") == 0 || strcmp(arg, "--help") == 0)){
-            *help = true;
-            return true;
-        }
         for (size_t i=0; i<args.optional_count; ++i){
             clags_opt_t opt = args.optional[i];
-            if (strcmp(arg, opt.flag) == 0){
+            if ((opt.short_flag != NULL && strcmp(arg, opt.short_flag) == 0) || (opt.long_flag != NULL && strcmp(arg, opt.long_flag) == 0)){
                 if (argc-index <= 1){
                     fprintf(stderr, "[ERROR] Optional flag %s requires argument!\n", arg);
                     return false;
                 }
                 if (opt.value != NULL) *opt.value = argv[++index];
                 goto next_arg;
+            } else if (opt.long_flag != NULL && strncmp(arg, opt.long_flag, strlen(opt.long_flag)) == 0){
+                char *value = arg+strlen(opt.long_flag);
+                if (*value++ == '='){
+                    if (*value == '\0'){
+                        fprintf(stderr, "[ERROR] Designated option assignment may not have an empty value: '%s'!\n", arg);
+                        return false;
+                    }
+                    if (opt.value != NULL) *opt.value = value;
+                    goto next_arg;
+                }
             }
         }
+        
         for (size_t i=0; i<args.flag_count; ++i){
-            clags_flag_t bol = args.flags[i];
-            if (strcmp(arg, bol.flag) == 0){
-                if (bol.value != NULL) *bol.value = true;
+            clags_flag_t flag = args.flags[i];
+            if ((flag.short_flag != NULL && strcmp(arg, flag.short_flag) == 0) || (flag.long_flag != NULL && strcmp(arg, flag.long_flag) == 0)){
+                if (flag.value != NULL) *flag.value = true;
+                if (flag.exit) return true;
                 goto next_arg;
             }
         }
+
         if (required_found >= args.required_count){
             fprintf(stderr, "[ERROR] Unknown additional argument: '%s'!\n", arg);
             return false;
         }
+        // TODO: support lists
         clags_req_t req = args.required[required_found++];
         if (req.value != NULL) *req.value = arg;
     next_arg:
@@ -179,33 +195,57 @@ void _clags_usage(const char *program_name, clags_arg_t *_args, size_t arg_count
 
     printf("Usage: %s", program_name);
     if (args.optional_count) printf(" [OPTIONS]");
-    printf(" [FLAGS]");
+    if (args.flag_count) printf(" [FLAGS]");
     for (size_t i=0; i<args.required_count; ++i){
         printf(" <%s>", args.required[i].name);
     }
     printf("\n");
+    
     if (args.required_count){
         printf("  Arguments:\n");
         for (size_t i=0; i<args.required_count; ++i){
             clags_req_t req = args.required[i];
-            printf("    %-16s : %s\n", req.name, req.description);
+            printf("    %*s : %s\n", CLAGS_USAGE_ALIGNMENT, req.name, req.description);
         }
     }
     if (args.optional_count){
         printf("  Options:\n");
         for (size_t i=0; i<args.optional_count; ++i){
             clags_opt_t opt = args.optional[i];
-            size_t buf_size = strlen(opt.flag) + strlen(opt.field_name) + 4;
-            char buf[buf_size];
-            snprintf(buf, buf_size, "%s <%s>", opt.flag, opt.field_name);
-            printf("    %-16s : %s\n", buf, opt.description);
+            if (opt.short_flag){
+                if (opt.long_flag){
+                    size_t buf_size = strlen(opt.short_flag) + strlen(opt.long_flag) + (opt.field_name? strlen(opt.field_name):0) + 6;
+                    char buf[buf_size];
+                    snprintf(buf, buf_size, "%s, %s(=)%s>", opt.short_flag, opt.long_flag, opt.field_name);
+                    printf("    %*s : %s\n", CLAGS_USAGE_ALIGNMENT, buf, opt.description);
+                } else{
+                    printf("    %*s : %s\n", CLAGS_USAGE_ALIGNMENT, opt.short_flag, opt.description);
+                }
+            }else if (opt.long_flag){
+                size_t buf_size = strlen(opt.long_flag) + (opt.field_name? strlen(opt.field_name):0) + 4;
+                char buf[buf_size];
+                snprintf(buf, buf_size, "%s(=)%s", opt.long_flag, opt.field_name);
+                printf("    %*s : %s\n", CLAGS_USAGE_ALIGNMENT, buf, opt.description);
+            }
         }
     }
-    printf("  Flags:\n");
-    for (size_t i=0; i<args.flag_count; ++i){
-        clags_flag_t bol = args.flags[i];
-        printf("    %-16s : %s\n", bol.flag, bol.description);
+    if (args.flag_count){
+        printf("  Flags:\n");
+        for (size_t i=0; i<args.flag_count; ++i){
+            clags_flag_t flag = args.flags[i];
+            if (flag.short_flag){
+                if (flag.long_flag){
+                    size_t buf_size = strlen(flag.short_flag) + strlen(flag.long_flag) + 12;
+                    char buf[buf_size];
+                    snprintf(buf, buf_size, "%s, %s", flag.short_flag, flag.long_flag);
+                    printf("    %*s : %s\n", CLAGS_USAGE_ALIGNMENT, buf, flag.description);
+                } else{
+                    printf("    %*s : %s\n", CLAGS_USAGE_ALIGNMENT, flag.short_flag, flag.description);
+                }
+            }else if (flag.long_flag){
+                printf("    %*s : %s\n", CLAGS_USAGE_ALIGNMENT, flag.long_flag, flag.description);
+            }
+        }
     }
-    printf("    %-16s : %s\n", "-h / --help", "print this help dialog");
 }
 #endif // CLAGS_IMPLEMENTATION
