8a864504f900d18ea672446d94f0ba06f5e64dd0
Constantijn de Meer <cdemeer@fim.uni-passau.de>
2025-12-26 17:11:00 +0100

    some minor example tweaks

--- examples/05_paths.c
+++ examples/05_paths.c
@@ -1,6 +1,6 @@
 /*
   Example 5: Paths
-      This is exactly the same example program as `01_basic`,
+      This is almost exactly the same example program as `01_basic`,
       only that here, we require a valid file input for the `input_file` argument 
 */
 
