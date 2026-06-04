1f2234d915eb8ec19aea3399a16d17c4e47e6ee0
Constantijn de Meer <fietecc@gmail.com>
2026-06-04 17:08:26 +0200

    update version to v3.0.1

--- clags.h
+++ clags.h
@@ -1,5 +1,5 @@
 /*
-  clags.h - Version 3.2.0 - https://github.com/fietec/clags.h
+  clags.h - Version 3.2.1 - https://github.com/fietec/clags.h
 
   A simple declarative command line arguments parser for C99
 
